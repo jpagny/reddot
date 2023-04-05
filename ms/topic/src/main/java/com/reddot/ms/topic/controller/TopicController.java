618aa7e1d1f5e99c64a8eea5ec3415430bf839b0
Jérôme Pagny <pagny.jerome@gmail.com>
2023-04-05 23:05:01 +0200

    Delete apache Camel

--- ms/topic/src/main/java/com/reddot/ms/topic/controller/TopicController.java
+++ ms/topic/src/main/java/com/reddot/ms/topic/controller/TopicController.java
@@ -5,10 +5,6 @@ import com.reddot.ms.topic.exception.ResourceNotFoundException;
 import com.reddot.ms.topic.service.implement.TopicServiceImplement;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.camel.CamelContext;
-import org.apache.camel.Route;
-import org.apache.camel.model.RouteDefinition;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,19 +12,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@RestController("topicController")
 @AllArgsConstructor
-@RequestMapping("/topic")
+@RequestMapping("/topics")
 @Slf4j
 public class TopicController {
 
-    @Autowired
-    CamelContext camelContext;
-
     private final TopicServiceImplement topicService;
+
+    @GetMapping(value = "/", produces = {"application/json"})
+    public ResponseEntity<List<TopicDTO>> getAll() {
+        List<TopicDTO> topics = topicService.getAll();
+        return new ResponseEntity<>(topics, HttpStatus.OK);
+    }
 
     @GetMapping(value = "/{id}", produces = {"application/json"})
     public ResponseEntity<TopicDTO> getTopicById(@PathVariable("id") Long id) {
@@ -48,24 +46,6 @@ public class TopicController {
 
         }
     }
-
-    @GetMapping(value = "/", produces = {"application/json"})
-    public ResponseEntity<List<TopicDTO>> getAll() {
-            List<TopicDTO> topics = topicService.getAll();
-            return new ResponseEntity<>(topics, HttpStatus.OK);
-    }
-
-
-
-    @GetMapping("/routes")
-    public List<String> getRoutes() throws Exception {
-        List<String> routes = new ArrayList<>();
-        for (Route route : camelContext.getRoutes()) {
-            routes.add(route.getId());
-        }
-        return routes;
-    }
-
 
 
 }
