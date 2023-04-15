c6f7384bb3cf1223518ef9f09980528d3471df19
Jérôme Pagny <pagny.jerome@gmail.com>
2023-04-15 17:04:11 +0200

    Add tests route

--- ms/topicManagementService/src/test/java/com/elysium/reddot/ms/topic/infrastructure/inbound/rest/route/TopicRouteBuilderTest.java
+++ ms/topicManagementService/src/test/java/com/elysium/reddot/ms/topic/infrastructure/inbound/rest/route/TopicRouteBuilderTest.java
@@ -22,11 +22,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -62,9 +64,10 @@ public class TopicRouteBuilderTest extends CamelTestSupport {
     @Test
     public void givenTopicsExist_whenRouteGetAllTopicsIsCalled_thenAllTopicsAreRetrieved() {
         // given
-        Exchange exchange = createExchangeWithBody(null);
         TopicDTO topic1 = new TopicDTO(1L, "name 1", "Name 1", "Topic 1");
         TopicDTO topic2 = new TopicDTO(2L, "name 2", "Name 2", "Topic 2");
+
+        Exchange exchange = new DefaultExchange(context);
 
         List<TopicDTO> topicList = Arrays.asList(topic1, topic2);
 
@@ -77,12 +80,12 @@ public class TopicRouteBuilderTest extends CamelTestSupport {
 
         // when
         Exchange responseExchange = template.send("direct:getAllTopics", exchange);
-        ApiResponseDTO actualApiResponse = responseExchange.getIn().getBody(ApiResponseDTO.class);
+        ApiResponseDTO actualResponse = responseExchange.getIn().getBody(ApiResponseDTO.class);
 
         // then
-        assertEquals(expectedApiResponse.getStatus(), actualApiResponse.getStatus());
-        assertEquals(expectedApiResponse.getMessage(), actualApiResponse.getMessage());
-        assertEquals(expectedApiResponse.getData(), actualApiResponse.getData());
+        assertEquals(expectedApiResponse.getStatus(), actualResponse.getStatus());
+        assertEquals(expectedApiResponse.getMessage(), actualResponse.getMessage());
+        assertEquals(expectedApiResponse.getData(), actualResponse.getData());
     }
 
 
@@ -90,37 +93,78 @@ public class TopicRouteBuilderTest extends CamelTestSupport {
     public void givenExistingTopic_whenRouteGetTopicByIdIsCalledWithValidId_thenCorrectTopicIsReturned() {
         // given
         Long topicId = 1L;
-        Exchange exchange = createExchangeWithBody(null);
+        TopicDTO topic = new TopicDTO(topicId, "name 1", "Name 1", "Topic 1");
+
+        Exchange exchange = new DefaultExchange(context);
         exchange.getIn().setHeader("id", topicId);
+
+        // expected
+        ApiResponseDTO expectedApiResponse = new ApiResponseDTO(HttpStatus.OK.value(),
+                "Topic with id 1 retrieved successfully", topic);
+
+        // mock
+        when(topicService.getTopicById(topicId)).thenReturn(topic);
 
         // when
         Exchange result = template.send(TopicRouteConstants.GET_TOPIC_BY_ID.getRouteName(), exchange);
-        ApiResponseDTO apiResponseDTO = result.getMessage().getBody(ApiResponseDTO.class);
+        ApiResponseDTO actualResponse = result.getMessage().getBody(ApiResponseDTO.class);
 
         // then
-        assertEquals(HttpStatus.OK.value(), apiResponseDTO.getStatus());
-        assertEquals("Topic with id " + topicId + " retrieved successfully", apiResponseDTO.getMessage());
+        assertEquals(expectedApiResponse.getStatus(), actualResponse.getStatus());
+        assertEquals(expectedApiResponse.getMessage(), actualResponse.getMessage());
+        assertEquals(expectedApiResponse.getData(), actualResponse.getData());
     }
 
     @Test
-    public void givenNonExistingTopicId_whenGetTopicByIdIsCalled_thenThrowResourceNotFoundException() {
+    public void givenNonExistingTopicId_whenRouteGetTopicByIdIsCalled_thenThrowResourceNotFoundException() {
         // given
         Long nonExistingId = 99L;
-        Exchange exchange = createExchangeWithBody(null);
+        Exchange exchange = new DefaultExchange(context);
         exchange.getIn().setHeader("id", nonExistingId);
+
+        // expected
+        ApiResponseDTO expectedApiResponse = new ApiResponseDTO(HttpStatus.NOT_FOUND.value(),
+                "The topic with ID 99 does not exist.", null);
 
         // mock
         when(topicService.getTopicById(nonExistingId)).thenThrow(new ResourceNotFoundException("topic", String.valueOf(nonExistingId)));
 
         // when
         Exchange result = template.send(TopicRouteConstants.GET_TOPIC_BY_ID.getRouteName(), exchange);
-        ApiResponseDTO apiResponseDTO = result.getMessage().getBody(ApiResponseDTO.class);
+        ApiResponseDTO actualResponse = result.getMessage().getBody(ApiResponseDTO.class);
 
-        // assert
-        assertEquals(HttpStatus.NOT_FOUND.value(), apiResponseDTO.getStatus());
-        assertEquals("The topic with ID " + nonExistingId + " does not exist.", apiResponseDTO.getMessage());
+        // then
+        assertEquals(expectedApiResponse.getStatus(), actualResponse.getStatus());
+        assertEquals(expectedApiResponse.getMessage(), actualResponse.getMessage());
+        assertEquals(expectedApiResponse.getData(), actualResponse.getData());
     }
 
+    @Test
+    public void givenValidTopic_whenRouteCreateTopicIsCalled_thenTopicIsCreated() {
+        // given
+        TopicDTO inputTopic = new TopicDTO(null, "name", "Name", "Description");
+        TopicDTO createdTopic = new TopicDTO(1L, inputTopic.getName(), inputTopic.getLabel(), inputTopic.getDescription());
+
+        Exchange exchange = new DefaultExchange(context);
+        exchange.getIn().setHeader(Exchange.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        exchange.getIn().setBody(inputTopic);
+
+        // expected
+        ApiResponseDTO expectedApiResponse = new ApiResponseDTO(HttpStatus.CREATED.value(),
+                "Topic with name " + createdTopic.getName() + " created successfully", createdTopic);
+
+        // mock
+        when(topicService.createTopic(inputTopic)).thenReturn(createdTopic);
+
+        // when
+        Exchange responseExchange = template.send(TopicRouteConstants.CREATE_TOPIC.getRouteName(), exchange);
+        ApiResponseDTO actualResponse = responseExchange.getMessage().getBody(ApiResponseDTO.class);
+
+        // then
+        assertEquals(expectedApiResponse.getStatus(), actualResponse.getStatus());
+        assertEquals(expectedApiResponse.getMessage(), actualResponse.getMessage());
+        assertEquals(expectedApiResponse.getData(), actualResponse.getData());
+    }
 
     @Test
     void givenTopicExists_whenRouteCreateTopicIsCalledWithCreatingSameTopic_thenResourceAlreadyExistExceptionHandlerIsTriggered() {
@@ -131,16 +175,127 @@ public class TopicRouteBuilderTest extends CamelTestSupport {
         exchange.getIn().setBody(existingTopic);
         exchange.getIn().setHeader(Exchange.HTTP_METHOD, "POST");
 
+        // expected
+        ApiResponseDTO expectedApiResponse = new ApiResponseDTO(HttpStatus.CONFLICT.value(),
+                "The topic with name 'name' already exists.", null);
+
         // mock
         when(topicService.createTopic(existingTopic)).thenThrow(new ResourceAlreadyExistException("topic", "name", "name"));
 
         // when
-        Exchange responseExchange = template.send("direct:createTopic", exchange);
-        ApiResponseDTO apiResponse = responseExchange.getMessage().getBody(ApiResponseDTO.class);
+        Exchange responseExchange = template.send(TopicRouteConstants.CREATE_TOPIC.getRouteName(), exchange);
+        ApiResponseDTO actualResponse = responseExchange.getMessage().getBody(ApiResponseDTO.class);
 
         // then
-        assertEquals(HttpStatus.CONFLICT.value(), apiResponse.getStatus());
-        assertEquals("The topic with name 'name' already exists.", apiResponse.getMessage());
+        assertEquals(expectedApiResponse.getStatus(), actualResponse.getStatus());
+        assertEquals(expectedApiResponse.getMessage(), actualResponse.getMessage());
+        assertEquals(expectedApiResponse.getData(), actualResponse.getData());
+    }
+
+    @Test
+    public void givenValidRequest_whenRouteUpdateTopicIsCalled_thenTopicIsUpdated() {
+        // given
+        Long topicId = 1L;
+        TopicDTO request = new TopicDTO(topicId, "newName", "newDescription", "newIcon");
+        TopicDTO updatedTopic = new TopicDTO(topicId, "newName", "newDescription", "newIcon");
+
+        Exchange exchange = new DefaultExchange(context);
+        exchange.getIn().setHeader("id", topicId);
+        exchange.getIn().setBody(request);
+
+        // expected
+        ApiResponseDTO expectedApiResponse = new ApiResponseDTO(HttpStatus.OK.value(),
+                "Topic with name " + updatedTopic.getName() + " updated successfully", updatedTopic);
+
+        // mock
+        when(topicService.updateTopic(topicId, request)).thenReturn(updatedTopic);
+
+        // when
+        Exchange responseExchange = template.send(TopicRouteConstants.UPDATE_TOPIC.getRouteName(), exchange);
+        ApiResponseDTO actualResponse = responseExchange.getMessage().getBody(ApiResponseDTO.class);
+
+        // then
+        assertEquals(expectedApiResponse.getStatus(), actualResponse.getStatus());
+        assertEquals(expectedApiResponse.getMessage(), actualResponse.getMessage());
+        assertEquals(expectedApiResponse.getData(), actualResponse.getData());
+    }
+
+    @Test
+    public void givenInvalidRequest_whenRouteUpdateTopicIsCalled_thenResourceNotFoundExceptionIsThrown() {
+        // given
+        Long nonExistingId = 99L;
+        TopicDTO request = new TopicDTO(nonExistingId, "newName", "newDescription", "newIcon");
+
+        Exchange exchange = new DefaultExchange(context);
+        exchange.getIn().setHeader("id", nonExistingId);
+        exchange.getIn().setBody(request);
+
+        ApiResponseDTO expectedApiResponse = new ApiResponseDTO(HttpStatus.NOT_FOUND.value(),
+                "The topic with ID 99 does not exist.", null);
+
+        // mock
+        when(topicService.updateTopic(nonExistingId, request)).thenThrow(new ResourceNotFoundException("topic", String.valueOf(nonExistingId)));
+
+        // when
+        Exchange result = template.send(TopicRouteConstants.UPDATE_TOPIC.getRouteName(), exchange);
+        ApiResponseDTO actualResponse = result.getMessage().getBody(ApiResponseDTO.class);
+
+        // then
+        assertEquals(expectedApiResponse.getStatus(), actualResponse.getStatus());
+        assertEquals(expectedApiResponse.getMessage(), actualResponse.getMessage());
+        assertEquals(expectedApiResponse.getData(), actualResponse.getData());
+    }
+
+    @Test
+    public void givenTopicExists_whenRouteDeleteTopicIsCalled_thenTopicIsDeleted() {
+        // given
+        Long topicId = 1L;
+        TopicDTO topicDTO = new TopicDTO(topicId, "test", "Test", "Test topic");
+
+        Exchange exchange = new DefaultExchange(context);
+        exchange.getIn().setHeader("id", topicId);
+
+        // expected
+        ApiResponseDTO expectedApiResponse = new ApiResponseDTO(HttpStatus.OK.value(),
+                "Topic with id " + topicId + " deleted successfully", topicDTO);
+
+        // mock
+        when(topicService.getTopicById(1L)).thenReturn(topicDTO);
+
+        // when
+        Exchange result = template.send(TopicRouteConstants.DELETE_TOPIC.getRouteName(), exchange);
+        ApiResponseDTO actualResponse = result.getMessage().getBody(ApiResponseDTO.class);
+
+        // assert
+        assertEquals(expectedApiResponse.getStatus(), actualResponse.getStatus());
+        assertEquals(expectedApiResponse.getMessage(), actualResponse.getMessage());
+        assertEquals(expectedApiResponse.getData(), actualResponse.getData());
+    }
+
+    @Test
+    public void givenInvalidRequest_whenRouteDeleteTopicIsCalled_thenResourceNotFoundExceptionIsThrown() {
+        // given
+        Long nonExistingId = 99L;
+
+        Exchange exchange = new DefaultExchange(context);
+        exchange.getIn().setHeader("id", nonExistingId);
+
+        // expected
+        ApiResponseDTO expectedApiResponse = new ApiResponseDTO(HttpStatus.NOT_FOUND.value(),
+                "The topic with ID 99 does not exist.", null);
+
+        // mock
+        doThrow(new ResourceNotFoundException("topic", String.valueOf(nonExistingId)))
+                .when(topicService).deleteTopicById(nonExistingId);
+
+        // when
+        Exchange result = template.send(TopicRouteConstants.DELETE_TOPIC.getRouteName(), exchange);
+        ApiResponseDTO actualResponse = result.getMessage().getBody(ApiResponseDTO.class);
+
+        // then
+        assertEquals(expectedApiResponse.getStatus(), actualResponse.getStatus());
+        assertEquals(expectedApiResponse.getMessage(), actualResponse.getMessage());
+        assertEquals(expectedApiResponse.getData(), actualResponse.getData());
     }
 
 
