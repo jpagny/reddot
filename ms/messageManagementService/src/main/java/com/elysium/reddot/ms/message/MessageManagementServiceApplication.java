b7d05bbc673c26cecba79dcd506a518d7619ff70
Jérôme Pagny <pagny.jerome@gmail.com>
2023-05-05 12:20:30 +0200

    Add new module : message

---
+++ ms/messageManagementService/src/main/java/com/elysium/reddot/ms/message/MessageManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elysium.reddot.ms.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageManagementServiceApplication.class, args);
+    }
+
+}
