eae71a1b144c3001909d3f50a565f37ce7f60043
Jérôme Pagny <pagny.jerome@gmail.com>
2023-05-05 07:47:42 +0200

    Add new module : thread

---
+++ ms/threadManagementService/src/test/java/com/elysium/reddot/ms/thread/ThreadManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elysium.reddot.ms.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
